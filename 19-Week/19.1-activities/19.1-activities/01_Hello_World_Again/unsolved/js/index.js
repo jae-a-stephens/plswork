91dc848f4f6d9f62d0cd6d8a0d6f2b997962b640
JaeStephens <jacobstephens@knights.ucf.edu>
2023-04-10 15:20:17 -0400

    update

---
+++ 19-Week/19.1-activities/19.1-activities/01_Hello_World_Again/unsolved/js/index.js
@@ -0,0 +1,9 @@
+// Create a variable below
+// var helloWorld = "Hello World!";
+
+var helloWorld = "You just never quit, do you? Took out Ghaul. Woke up the Traveler. And now half of what i hear in the streets is how much you and your clan are making a difference. And that's why i started this whole clan thing in the first place. People are still waiting for the Vanguard to lead the way. But it's time for a change! And guardians like you are making it happen. No pressure.";
+
+// Console.log() your variable here.
+// console.log(helloWorld);
+
+console.log(helloWorld);
---
+++ 19-Week/19.1-activities/19.1-activities/02_More_Variables/unsolved/js/index.js
@@ -0,0 +1,40 @@
+// Create the addNumbers function here.
+// function addNumbers() {};
+    // between {} add the folowing tasks:
+        // var tow = 2;
+        // var three = 3;
+        // var five = two + three;
+        // console.log(five); 
+function addNumbers() {
+    var two = 2;
+    var three = 3;
+    var five = two + three;
+    console.log(five);
+}
+
+addNumbers();
+
+// After {} this line should be out side of the function
+    // addNumbers();
+
+
+// Step 2: Perform String Concatenation
+// Create the stringConcat function here.
+// function stringConcat() {};
+    // between {} add the folowing tasks:
+        // var taco = 'taco';
+        // var cat = 'cat';
+        // var tacoCat = taco + ' ' + cat;
+        // alert(tacoCat);
+function stringConcat() {
+    var taco = 'taco';
+    var cat = 'cat';
+    var tacoCat = taco + ' ' + cat;
+    alert(tacoCat);
+}
+
+stringConcat();
+
+
+// After {} this line should be out side of the function
+    // stringConcat();
